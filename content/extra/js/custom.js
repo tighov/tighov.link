5ec3549c883dd62f84eed0151d1b2589a4bdb073
Tigran <email@thetigran.com>
2025-05-29 23:37:09 +0400

    fixing CORS for tighov.link

--- content/extra/js/custom.js
+++ content/extra/js/custom.js
@@ -41,7 +41,7 @@ $(function () {
         contentType: "application/json",
         headers: {
           "accept": "application/json",
-          "Access-Control-Allow-Origin": "https://www.tighov.link"
+          "Access-Control-Allow-Origin": "'https://www.tighov.link,https://tighov.link'"
         },
         beforeSend: function (e) {
           $("#contact-form-submit").val("Submitting...")
@@ -110,7 +110,7 @@ $(function () {
         contentType: "application/json",
         headers: {
           "accept": "application/json",
-          "Access-Control-Allow-Origin": "https://www.tighov.link"
+          "Access-Control-Allow-Origin": "'https://www.tighov.link,https://tighov.link'"
         },
         beforeSend: function (e) {
           $("#subscribe-button").val("Joining...")
--- scripts/invalidate_cdn.sh
+++ scripts/invalidate_cdn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-CDN_DISTRIBUTION_ID=E1NP7SCKKPDT8J
+CDN_DISTRIBUTION_ID=E3VZIDXD1BB580
 aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID --paths "/*"
