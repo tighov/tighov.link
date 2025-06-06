a856d20a7e3febf79cbaba6609a839867b9d7f88
Tigran <email@thetigran.com>
2025-06-07 01:45:38 +0400

    python3.9 -> python3.13

--- terraform/aws/modules/subscribe-from/lambda.tf
+++ terraform/aws/modules/subscribe-from/lambda.tf
@@ -1,46 +1,7 @@
 data "archive_file" "lambda_zip_file" {
   type        = "zip"
-  source_file = "${path.module}/lambdas/contact_form.py"
-  output_path = "${path.module}/lambdas/contact_form.zip"
-}
-
-data "archive_file" "options_lambda_zip_file" {
-  type        = "zip"
-  source_file = "${path.module}/lambdas/options_dynamic_origin.py"
-  output_path = "${path.module}/lambdas/options_dynamic_origin.zip"
-}
-
-resource "aws_iam_role" "lambda_role" {
-  name               = "lambda_role"
-  assume_role_policy = file("${path.module}/lambdas/lambda_assume_role_policy.json")
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_exec_role_attachment" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_policy" "lambda_ses_send_email" {
-  name        = "lambda-ses-send-email"
-  description = "Allow Lambda to send email via SES"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ses:SendEmail",
-          "ses:SendRawEmail"
-        ]
-        Resource = "arn:aws:ses:us-east-1:209479297820:identity/email@thetigran.com"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_ses_send_email_attachment" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_ses_send_email.arn
+  source_file = "${path.module}/lambdas/subscribe_form.py"
+  output_path = "${path.module}/lambdas/subscribe_form.zip"
 }
 
 resource "aws_lambda_function" "contact_form_lambda_function" {
---
+++ terraform/aws/modules/subscribe-from/lambdas/subscribe_form.py
@@ -0,0 +1,131 @@
+import boto3
+from botocore.exceptions import ClientError
+import json
+import os
+import time
+import uuid
+import base64
+
+client = boto3.client('ses')
+sender = os.environ['SENDER_EMAIL']
+sendto = os.environ['SENDTO_EMAIL']
+configset = os.environ['CONFIG_SET']
+domain_name = os.environ['DOMAIN_NAME']
+charset = 'UTF-8'
+
+dynamodb = boto3.resource('dynamodb')
+
+def lambda_handler(event, context):
+
+    origin = event['headers'].get('origin')
+    allowed_origins = ["https://www." + domain_name, "https://" + domain_name]
+    if origin in allowed_origins:
+        cors_origin = origin
+    else:
+        cors_origin = "https://www." + domain_name  # fallback
+
+    headers = {
+        "Access-Control-Allow-Origin": cors_origin,
+        "Access-Control-Allow-Methods": "POST,OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type"
+    }
+
+    if event['httpMethod'] == 'POST':
+        return sendMail(event, context, headers)
+    else:
+        return {
+            "statusCode": 405,
+            "headers": headers,
+            "body": json.dumps("Method Not Allowed")
+        }
+
+def sendMail(event, context, headers):
+    try:
+        data = event['body']
+        data = json.loads(data)
+        name = data['name']
+        email = data['email']
+        message = data['message']
+        subject = data['subject']
+        table_name = base64.b64decode(data['key']).decode()
+
+        emailsbj = table_name + ' -- ' + subject
+        content = emailsbj + \
+                  ',\n\r' + \
+                  '\n\rMessage from: ' + name + \
+                  '\n\rEmail: ' + email + '\n\r' \
+                  + message
+       
+        # table = dynamodb.Table(table_name)
+        # saveToDynamoDB(data, table)
+        response = sendMailToUser(data, emailsbj, content)
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return {
+            "statusCode": 501,
+            "headers": headers,
+            "body": json.dumps({"error": e.response['Error']['Message']})
+        }
+    else:
+        print("Email sent! Message Id:"),
+        print(response['MessageId'])
+    return {
+        "statusCode": 200,
+        "headers": headers,
+        "body": json.dumps({"message": "OK"})
+    }
+
+def list(event, context, headers):
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # fetch all records from database
+    result = table.scan()
+
+    #return response
+    return {
+        "statusCode": 200,
+        "headers": headers,
+        "body": json.dumps(result['Items'])
+    }
+
+def saveToDynamoDB(data, table):
+    timestamp = int(time.time() * 1000)
+    # Insert details into DynamoDB Table
+    item = {
+        'id': str(uuid.uuid1()),
+        'name': data['name'],
+        'subject': data['subject'],
+        'email': data['email'],
+        'message': data['message'],
+        'createdAt': timestamp,
+        'updatedAt': timestamp
+    }
+    table.put_item(Item=item)
+    return
+
+def sendMailToUser(data, subject, content):
+    # Send Email using SES
+    return client.send_email(
+        Source=sender,
+        Destination={
+            'ToAddresses': [
+                sendto,
+            ],
+        },
+        Message={
+            'Subject': {
+                'Charset': charset,
+                'Data': subject
+            },
+            'Body': {
+                'Html': {
+                    'Charset': charset,
+                    'Data': content
+                },
+                'Text': {
+                    'Charset': charset,
+                    'Data': content
+                }
+            }
+        }
+    )
